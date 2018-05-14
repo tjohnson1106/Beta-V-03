72ccea81306e6cf0526cbb960322fc526be791a0
Thomas Johnson <thomasjohnson@Thomass-iMac.local>
2018-05-13 20:53:47 -0400

    created post type gql objects

--- lib/sagaBetaV3_web/router.ex
+++ lib/sagaBetaV3_web/router.ex
@@ -2,10 +2,16 @@ defmodule SagaBetaV3Web.Router do
   use SagaBetaV3Web, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/api", SagaBetaV3Web do
-    pipe_through :api
+    pipe_through(:api)
+
+    forward("/graphql", Absinthe.Plug, schema: SagaBetaV3Web.Schema)
+
+    if Mix.env() == :dev do
+      forward("/graphiql", Absinthe.Plug.GraphiQL, schema: SagaBetaV3Web.Schema)
+    end
   end
 end
---
+++ lib/sagaBetaV3_web/views/schema.ex
@@ -0,0 +1,11 @@
+defmodule SagaBetaV3Web.Schema do 
+    use Absinthe.Schema
+
+
+    query do
+        
+    end
+
+
+
+end
